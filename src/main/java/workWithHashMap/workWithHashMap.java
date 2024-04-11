9c427c395a6a03b66e7012ddbe9960a8ae0578e6
EvilDroid <aikelon@gmail.com>
2024-04-11 23:43:06 +0400

    Commit

---
+++ src/main/java/workWithHashMap/workWithHashMap.java
@@ -0,0 +1,36 @@
+package workWithHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class workWithHashMap {
+
+
+    public static void main(String[] args) {
+        Map<String, String> countries = new HashMap<String, String>();
+
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+
+        //get value by key
+        countries.get("UA");
+
+        //get key by value
+        String key = "";
+        for (Map.Entry <String, String> entry : countries.entrySet()) {
+            if (entry.getValue().equals("Ukraine")) {
+                key = entry.getKey();
+                break;
+            }
+        }
+        System.out.println(key);
+    }
+
+
+
+
+
+
+
+}
